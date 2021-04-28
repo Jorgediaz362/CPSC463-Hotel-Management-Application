9c3796e9695a5d93bc7a02dd0e7ae39934eee8d8
david810376 <33507197+david810376@users.noreply.github.com>
2021-04-29 01:04:45 +0800

    small change for guest_information part
    change firstname to firstName and change the place

--- functions.php
+++ functions.php
@@ -15,7 +15,7 @@
         
     //==insert into the database======
     //connecting to the database ============================
-        @$db = new mysqli('localhost:9000', 'root', 'root', 'test');
+        @$db = new mysqli('mariadb', 'cs431s24', 'eiY2ahm1', 'cs431s24');
 
         if (mysqli_connect_errno()) {
             echo "<p>Error: Could not connect to database.<br/>
