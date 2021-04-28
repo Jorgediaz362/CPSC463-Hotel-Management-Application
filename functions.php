f6c86bdcc5fdd49effc81a39bdc40cb2ce17befe
david810376 <33507197+david810376@users.noreply.github.com>
2021-04-29 00:44:35 +0800

    Update functions.php

--- functions.php
+++ functions.php
@@ -148,8 +148,8 @@
         for($row = 0; $row < count($array); $row++){         
           echo "
                  <tr class='gradeX'>
-                <td>".$array[$row]['firstname']."</td>
-                <td>".$array[$row]['lastname']."</td>
+                <td>".$array[$row]['firstName']."</td>
+                <td>".$array[$row]['lastName']."</td>
                 <td>".$array[$row]['stateID']."</td>
                 <td>".$array[$row]['phone']."</td>
                 <td>".$array[$row]['email']."</td>
@@ -165,8 +165,7 @@
           for($row = 0; $row < count($array); $row++){         
             echo "
                 <tr class='gradeX'>
-                    <td>".$array[$row]['firstname']."</td>
-                    <td>".$array[$row]['lastname']."</td>
+                    <td>".$array[$row]['firstName']." ".$array[$row]['lastName']."</td>
                     <td>".$array[$row]['checkinDate']."</td>
                     <td>".$array[$row]['checkoutDate']."</td>
                     <td>".$array[$row]['roomType']."</td>
