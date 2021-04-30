a9116daaffc310f005d87c1c00e89fe826f3d49a
david810376 <33507197+david810376@users.noreply.github.com>
2021-04-30 18:26:44 +0800

    Update dailyreport.php

--- dailyreport.php
+++ dailyreport.php
@@ -262,10 +262,10 @@
                         <a href="tab-panel.html"><i class="fa fa-qrcode"></i> Housekeeping</a>
                     </li>                                      
                     <li>
-                        <a href="guest_information.html"><i class="fa fa-edit"></i> Guest Information</a>
+                        <a href="guest_information.php"><i class="fa fa-edit"></i> Guest Information</a>
                     </li>
                     <li>
-                        <a href="current_stay.html"><i class="fa fa-fw fa-file"></i> Guest Stay</a>
+                        <a href="current_stay.php"><i class="fa fa-fw fa-file"></i> Guest Stay</a>
                     </li>
                     <li>
                         <a href="searchguest.php"><i class="fa fa-fw fa-file"></i> Search for a Guest</a>
